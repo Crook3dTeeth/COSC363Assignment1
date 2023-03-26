de129573aeeca4ecc8b877228a8840d2bd0f608c
PogoPlayering <tom.kirkwood2002@gmail.com>
2023-03-26 21:50:30 +1300

    Added stuff

--- COSC363/Model3D.cpp
+++ COSC363/Model3D.cpp
@@ -5,14 +5,30 @@
 //  See Lab01.pdf for details
 //  ========================================================================
 
-#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <climits>
+#include <math.h> 
 #include <GL/freeglut.h>
+using namespace std;
 
+
+// Camera Position data
 int cam_hgt = 10;
 int cam_x = 0;
 int cam_z = 0;
-int cam_rotation = 0;
+int cam_look_x = 0;
+int cam_look_z = 0;
+int cam_look_y = 0;
 float theta = 0;
+
+// Mesh file data
+float* x, * y, * z;					//vertex coordinates
+int* nv, * t1, * t2, * t3, * t4;		//number of vertices and vertex indices of each face
+int nvert, nface;					//total number of vertices and faces
+
+// Scene rendering variables
+int view_number = 0;
 
 // Moves the cam up and down using keyboard input
 void special(int key, int x, int y)
@@ -20,26 +36,77 @@ void special(int key, int x, int y)
 	switch (key) {
 	case GLUT_KEY_UP:
 		// move camera forward
-		cam_x++;
+		cam_x+= 0.1;
 		glutPostRedisplay();
 		break;
 	case GLUT_KEY_DOWN:
 		// move camera back
-		cam_x--;
+		cam_x-= 0.1;
 		glutPostRedisplay();
 		break;
 	case GLUT_KEY_LEFT:
 		//cam left
-		cam_rotation++;
+		theta--;
 		glutPostRedisplay();
 		break;
 	case GLUT_KEY_RIGHT:
 		// cam right
-		cam_rotation--;
+		theta++;
+		glutPostRedisplay();
+		break;
+	case GLUT_KEY_PAGE_UP:
+		cam_hgt++;
+		glutPostRedisplay();
+		break;
+	case GLUT_KEY_PAGE_DOWN:
+		cam_hgt--;
 		glutPostRedisplay();
 		break;
 	}
 }
+
+
+//-- Loads mesh data in OFF format    -------------------------------------
+void loadMeshFile(const char* fname)
+{
+	ifstream fp_in;
+	int ne;
+
+	fp_in.open(fname, ios::in);
+	if (!fp_in.is_open()) {
+		cout << "Error opening mesh file" << endl;
+		exit(1);
+	}
+
+	fp_in.ignore(INT_MAX, '\n');				//ignore first line
+	fp_in >> nvert >> nface >> ne;			    // read number of vertices, polygons, edges (not used)
+
+	x = new float[nvert];                        //create arrays
+	y = new float[nvert];
+	z = new float[nvert];
+
+	nv = new int[nface];
+	t1 = new int[nface];
+	t2 = new int[nface];
+	t3 = new int[nface];
+	t4 = new int[nface];
+
+	for (int i = 0; i < nvert; i++)                         //read vertex list 
+		fp_in >> x[i] >> y[i] >> z[i];
+
+	for (int i = 0; i < nface; i++)                         //read polygon list 
+	{
+		fp_in >> nv[i] >> t1[i] >> t2[i] >> t3[i];
+		if (nv[i] == 4)
+			fp_in >> t4[i];
+		else
+			t4[i] = -1;
+	}
+
+	fp_in.close();
+	cout << " File successfully read." << endl;
+}
+
 
 // Moves to the art places
 void keyboard_input(unsigned char key, int x, int y)
@@ -47,68 +114,171 @@ void keyboard_input(unsigned char key, int x, int y)
 	switch (key) {
 		case '0':
 			// Gallery view
+			view_number = 0;
+			cam_hgt = 10;
+			cam_x = 0;
+			cam_z = 0;
+			cam_look_x = 0;
+			cam_look_z = 0;
+			cam_look_y = 0;
+			theta = 0;
 			glutPostRedisplay();
 			break;
 		case '1':
 			// AAO-1 view
+			loadMeshFile("AmesWindow.off");
+
+			view_number = 1;
+			cam_hgt = 10;
+			cam_x = 0;
+			cam_z = 0;
+			cam_look_x = 0;
+			cam_look_z = 0;
+			cam_look_y = 0;
+			theta = 0;
 			glutPostRedisplay();
 			break;
 		case '2':
 			// AAO-2 view
+			loadMeshFile("square.off");
+
+			view_number = 2;
+			cam_hgt = 10;
+			cam_x = 0;
+			cam_z = 0;
+			cam_look_x = 0;
+			cam_look_z = 0;
+			cam_look_y = 0;
+			theta = 0;
 			glutPostRedisplay();
 			break;
 		case '3':
 			// AAO-3 view
+			loadMeshFile("square.off");
+
+			view_number = 3;
+			cam_hgt = 10;
+			cam_x = 0;
+			cam_z = 0;
+			cam_look_x = 0;
+			cam_look_z = 0;
+			cam_look_y = 0;
+			theta = 0;
 			glutPostRedisplay();
 			break;
 	}
 }
 
 
-
-//--Draws a grid of lines on the floor plane -------------------------------
+// Draws the checkered floor for the gallery
 void drawFloor()
 {
-	glColor3f(0., 0.5, 0.);			//Floor colour
+	bool flag = false;
+	int floor_height = -1;
+	float tile_width = 2;
+	float floor_width = 30;
 
-	for (int i = -50; i <= 50; i++)
-	{
-		glBegin(GL_LINES);			//A set of grid lines on the xz-plane
-		glVertex3f(-50, -0.75, i);
-		glVertex3f(50, -0.75, i);
-		glVertex3f(i, -0.75, -50);
-		glVertex3f(i, -0.75, 50);
-		glEnd();
+	glBegin(GL_QUADS);
+	glNormal3f(0, 1, 0);
+	for (int x = -floor_width; x <= floor_width; x += tile_width) {
+		for (int z = -floor_width; z <= floor_width; z += tile_width) {
+			if (flag) glColor3f(1.0, 1.0, 0.5);
+			else glColor3f(1.0, 0.5, 1.0);
+
+			glVertex3f(x, floor_height, z);
+			glVertex3f(x, floor_height, z + tile_width);
+			glVertex3f(x + tile_width, floor_height, z + tile_width);
+			glVertex3f(x + tile_width, floor_height, z);
+			flag = !flag;
+		}
 	}
+	glEnd();
 }
 
 
-//--Display: ---------------------------------------------------------------
-//--This is the main display module containing function calls for generating
-//--the scene.
+//--Function to compute the normal vector of a triangle with index indx ----------
+void normal(int indx)
+{
+	float x1 = x[t1[indx]], x2 = x[t2[indx]], x3 = x[t3[indx]];
+	float y1 = y[t1[indx]], y2 = y[t2[indx]], y3 = y[t3[indx]];
+	float z1 = z[t1[indx]], z2 = z[t2[indx]], z3 = z[t3[indx]];
+	float nx, ny, nz;
+	nx = y1 * (z2 - z3) + y2 * (z3 - z1) + y3 * (z1 - z2);
+	ny = z1 * (x2 - x3) + z2 * (x3 - x1) + z3 * (x1 - x2);
+	nz = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+	glNormal3f(nx, ny, nz);
+}
+
+
 void display(void)
 {
-	float lpos[4] = { 0., 10., 10., 1.0 };  //light's position
+	float light_pos[4] = { 0., 10., 10., 1.0 };  //light's position
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	gluLookAt(cam_x, cam_hgt, 2, 0, 0, 0, 0, 1, 0);  //Camera position and orientation
+	float radians = theta * 3.1415 / 180;
+	gluLookAt(12 * sin(radians), cam_hgt, 12 * cos(radians), cam_look_x, 0, cam_look_z, 0, 1, 0);  //Camera position and orientation
+	
+	switch (view_number) {
+	case 0:
+		// Galary view draw checkered floor
+		drawFloor();
+		glLightfv(GL_LIGHT0, GL_POSITION, light_pos);   //Set light position
 
-	glLightfv(GL_LIGHT0, GL_POSITION, lpos);   //Set light position
+		drawFloor();
 
-	glDisable(GL_LIGHTING);			//Disable lighting when drawing floor.
-	drawFloor();
+		glEnable(GL_LIGHTING);			//Enable lighting when drawing the teapot
+		glColor3f(0.0, 1.0, 1.0);
 
-	glEnable(GL_LIGHTING);			//Enable lighting when drawing the teapot
-	glColor3f(0.0, 1.0, 1.0);
 
-	glTranslatef(2, 1, -3);
-	glutSolidCube(1);
-	glRotatef(30, 0, 1, 0);
-	glutSolidTeapot(1);
+		// Draws objects
+		glTranslatef(0, 5, 0);
+		glutSolidCube(1);
+		glRotatef(30, 0, 1, 0);
+		glutSolidTeapot(1);
+		break;
+	case 1:
+		// Lighting
+		glLightfv(GL_LIGHT0, GL_POSITION, light_pos);   //Set light position
 
+		glEnable(GL_LIGHTING);			//Enable lighting when drawing the teapot
+		glColor3f(0.0, 1.0, 1.0);
+		glScalef(10, 10, 0);
+		
+		// Draws object
+		for (int indx = 0; indx < nface; indx++)		//draw each face
+		{
+			normal(indx);
+			if (nv[indx] == 3) {
+				glBegin(GL_TRIANGLES);
+			}
+			else {
+				glBegin(GL_QUADS);
+			}
+			glVertex3d(x[t1[indx]], y[t1[indx]], z[t1[indx]]);
+			glVertex3d(x[t2[indx]], y[t2[indx]], z[t2[indx]]);
+			glVertex3d(x[t3[indx]], y[t3[indx]], z[t3[indx]]);
+			if (nv[indx] == 4)
+				glVertex3d(x[t4[indx]], y[t4[indx]], z[t4[indx]]);
+			
+			glEnd();
+		}
+		
+		break;
+	case 2:
+		glBegin(GL_QUADS);
+		glVertex3f(0, 0, 0);
+		glVertex3f(1, 0, 0);
+		glVertex3f(1, 1, 0);
+		glVertex3f(0, 1, 0);
+		glEnd();
+		break;
+	case 3:
+		break;
+	}
+	
 	glFlush();
 }
 
@@ -116,6 +286,7 @@ void display(void)
 void initialize(void)
 {
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	
 
 	glEnable(GL_LIGHTING);		//Enable OpenGL states
 	glEnable(GL_LIGHT0);
@@ -125,7 +296,7 @@ void initialize(void)
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(50, 1, 10.0, 1000.0);   //Camera Frustum
+	gluPerspective(80, 1, 1.0, 1000.0);   //Camera Frustum
 }
 
 
@@ -136,13 +307,16 @@ int main(int argc, char** argv)
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_DEPTH);
 	glutInitWindowSize(600, 600);
 	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Teapot");
+	glutCreateWindow("Assignment 1");
+	
 	// SpecialKeys
 	glutSpecialFunc(special);
+	// Keyboard input
 	glutKeyboardFunc(keyboard_input);
 
 	initialize();
 	glutDisplayFunc(display);
+
 	glutMainLoop();
 	return 0;
 }
